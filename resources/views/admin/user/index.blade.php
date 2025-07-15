26589b4ed036bb9a0895294339887453801f9c10
UsmanGhani552 <usman.centosquare@gmail.com>
2025-07-15 12:46:13 +0500

    admin conditon check on delete

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,61 +1,68 @@
 @extends('admin.layout.master')
 @section('content')
-<div class="row">
-    <div class="col-sm-12">
-        <div class="page-title-box">
-            <div class="btn-group float-right">
-                <a type="button" href="{{ route('admin.user.create') }}" class="btn btn-primary waves-effect waves-light text-white"><i class="fas fa-plus-square mr-2"></i>Create</a>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-title-box">
+                <div class="btn-group float-right">
+                    <a type="button" href="{{ route('admin.user.create') }}"
+                        class="btn btn-primary waves-effect waves-light text-white"><i
+                            class="fas fa-plus-square mr-2"></i>Create</a>
+                </div>
+                <h4 class="page-title">Categories</h4>
             </div>
-            <h4 class="page-title">Categories</h4>
         </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            @if ($message = Session::get('success'))
-            <div class="alert alert-success alert-block">
-                <strong>{{ $message }}</strong>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                @if ($message = Session::get('success'))
+                    <div class="alert alert-success alert-block">
+                        <strong>{{ $message }}</strong>
+                    </div>
+                @endif
+                <div class="card-body">
+
+
+                    <table id="datatable" class="table table-striped table-bordered dt-responsive nowrap"
+                        style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Type</th>
+                                <th>Icon</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+
+
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>{{ $user->type }}</td>
+                                    <td><img src="{{ asset('images/user/' . $user->image) }}" alt="" class="thumb-sm mr-1"></td>
+                                    <td>
+                                        <a type="button" href="{{ route('admin.user.edit', $user->id) }}"
+                                            class="btn btn-sm btn-success text-white">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                        @if($user->type !== 'admin')
+                                            <a type="button" href="{{ route('admin.user.delete', $user->id) }}"
+                                                class="btn btn-sm btn-danger text-white">
+                                                <i class="fas fa-trash-alt"></i>
+                                            </a>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+
+                </div>
             </div>
-            @endif
-            <div class="card-body">
-
-
-                <table id="datatable" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Type</th>
-                            <th>Icon</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-
-
-                    <tbody>
-                        @foreach ($users as $user)
-                        <tr>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td>{{ $user->type }}</td>
-                            <td><img src="{{ asset('images/user/'.$user->image) }}" alt="" class="thumb-sm mr-1"></td>
-                            <td>
-                                <a type="button" href="{{ route('admin.user.edit',$user->id) }}" class="btn btn-sm btn-success text-white">
-                                    <i class="fas fa-edit"></i>
-                                </a>
-                                <a type="button" href="{{ route('admin.user.delete',$user->id) }}" class="btn btn-sm btn-danger text-white">
-                                    <i class="fas fa-trash-alt"></i>
-                                </a>
-                            </td>
-                        </tr>
-                        @endforeach
-
-                    </tbody>
-                </table>
-
-            </div>
-        </div>
-    </div> <!-- end col -->
-</div> <!-- end row -->
+        </div> <!-- end col -->
+    </div> <!-- end row -->
 @endsection
